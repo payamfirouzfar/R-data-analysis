9eb643144db9ce7d55032dabc382357faf757097
Payam Firouzfar <78821356+payamfirouzfar@users.noreply.github.com>
2021-08-25 20:24:22 -0700

    Add files via upload

---
+++ hh.R
@@ -0,0 +1,22 @@
+###bjgfbgj####
+#rbrgbrbrgb----
+
+x = 5  
+x = 6#kvfvfdv
+
+v1 <- c(1,2,3)
+mean(v1)
+min(v1)
+log
+ls()
+d <- ls()
+d
+d <- 1
+d
+rm(list = (ls()))
+?ls
+?read.csv()
+
+m <- matrix(c(4,15,6,12) , nrow = 2)
+m
+fisher.test(m)
